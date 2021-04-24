c3eabee104d9a3b476436b7af4c50905f40099c1
unknown <murakhas@list.ru>
2021-04-24 22:01:01 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontSize: 25 }}>Белогорская школа #4</Text>
+      <Image
+        source={{
+          uri:
+            "https://sun9-30.userapi.com/impg/jJD4lbUQuHYNHDvlBlCI_Rrqvhy33UAFPBCFxg/Z-OLVyuInRo.jpg?size=1200x1600&quality=96&sign=496f41d25263049b065e1a4fe237764d&type=album",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <Text>test soasdaasdsd ya sobaka</Text>
+      <Text>test soasdaasdsd ya sobaka</Text>
+      <Text>test soasdaasdsd ya sobaka</Text>
+      <Text>test soasdaasdsd ya sobaka</Text>
+      <Button title="Новости"></Button>
+      <Button title="Сотрудники"></Button>
+      <Button title="Питание"></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +28,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "#0000",
   },
 });
